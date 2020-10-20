6d438416783fa669dc122635aa3e68f8fb14d2e5
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-20 16:17:54 +0300

    switch-1

--- java tests/osnovi/switch/1/script.js
+++ java tests/osnovi/switch/1/script.js
@@ -19,8 +19,7 @@ let name = prompt('Введите название браузера');
 
 if (name == 'Edge') {
     alert('You have got the Edge!');
-}
-if (name == 'Chrome' || name == 'Firefox' || name == 'Safari' || name == 'Opera') {
+} else if (name == 'Chrome' || name == 'Firefox' || name == 'Safari' || name == 'Opera') {
     alert('Okay we support these browsers too');
 } else {
     alert('We hope that this page looks ok!');
