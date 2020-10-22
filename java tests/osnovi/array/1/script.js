bfaafdc25850f9eb59b52a9dd327e92bbdedd8d7
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-22 16:38:35 +0300

    array-1

---
+++ java tests/osnovi/array/1/script.js
@@ -0,0 +1,21 @@
+const arr = [`Джаз`, `Блюз`];
+console.log(arr);
+arr.push(`Рок-н-ролл`);
+console.log(arr);
+arr[1] = ('Классика');
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift('Регги');
+console.log(arr);
+arr.unshift('Рэп');
+console.log(arr);
+
+
+
+
+
+//arr.forEach(function(item, i, arr) {
+//  console.log(`${i}: ${item} внутри массива ${arr}`)
+
+//});
