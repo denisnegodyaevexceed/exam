45a2c6265d05dc8ca1d00a325adf84f208215c0a
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-26 14:46:49 +0300

    methods-array

---
+++ java tests/osnovi/array-methods/11/script.js
@@ -0,0 +1,31 @@
+// Получить средний возраст
+// важность: 4
+// Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+// Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+// Например:
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+
+// let arr = [ vasya, petya, masha ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+
+
+// function getAverageAge(users) {
+//     return users.reduce((sum, user) => sum + user.age, 0) / users.length;
+// }
+
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [vasya, petya, masha];
+
+let x = Math.round(getAverageAge(arr))
+alert(x);
