dd65e6418b9d70081ae588dee16166943ebe9783
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-26 16:08:03 +0300

    keys-values-entries

--- java tests/osnovi/keys-values-entries/1/script.js
+++ java tests/osnovi/keys-values-entries/1/script.js
@@ -1,5 +1,21 @@
-let sum = 0;
+// Сумма свойств объекта
+// важность: 5
+// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
 
+// Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+
+// Если объект salaries пуст, то результат должен быть 0.
+
+// Например:
+
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+alert(sumSalaries(salaries)); // 650
+let sum = 0;
 
 function sumSalaries(salaries) {
     for (let key of Object.values(salaries)) {
@@ -7,9 +23,6 @@ function sumSalaries(salaries) {
     }
     return sum;
 }
-
-
-
 let salaries = {
     "John": 100,
     "Pete": 300,
--- java tests/osnovi/keys-values-entries/2/script.js
+++ java tests/osnovi/keys-values-entries/2/script.js
@@ -1,6 +1,22 @@
-function count(obj) {
-    return Object.keys(obj).length;
+// Подсчёт количества свойств объекта
+// важность: 5
+// Напишите функцию count(obj), которая возвращает количество свойств объекта:
 
+// let user = {
+//   name: 'John',
+//   age: 30
+// };
+
+// alert( count(user) ); // 2
+// Постарайтесь сделать код как можно короче.
+
+// P.S. Игнорируйте символьные свойства, подсчитывайте только «обычные».
+
+function count(obj) {
+
+    let arr = Object.keys(obj);
+
+    return arr.length;
 }
 
 let user = {
