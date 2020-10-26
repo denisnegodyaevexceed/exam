e67c84f12cdf93392e6774a25b6cc2953a399249
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-26 13:56:39 +0300

    methods-array

--- java tests/osnovi/array-methods/8/script.js
+++ java tests/osnovi/array-methods/8/script.js
@@ -1,3 +1,22 @@
+// Отсортировать пользователей по возрасту
+// важность: 5
+// Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+// Например:
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+
 let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
 let petya = { name: "Петя", surname: "Иванов", id: 2 };
 let masha = { name: "Маша", surname: "Петрова", id: 3 };
@@ -11,5 +30,5 @@ let usersMapped = users.map(user => ({
 
 
 
-alert(usersMapped[0].id) // 1
-alert(usersMapped[0].fullName) // Вася Пупкин
+alert(usersMapped[0].id)
+alert(usersMapped[0].fullName)
