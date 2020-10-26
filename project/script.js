40773c7e2a75dc7463ba4448eb95f1ee624acf4f
Denis Negodyaev <dnegodyaev.exceedteam@gmail.com>
2020-10-26 17:51:17 +0300

    destructuring-assignment

--- project/script.js
+++ project/script.js
@@ -51,17 +51,10 @@ async function getResponse(i) {
 
 
 }
-
-
-
-
-
-
-
 next.addEventListener("click", function() {
 
-    if (j >= 9) {
-        document.querySelector('.next').innerHTML = ''; // удалили DIV
+    if (j >= 1000) {
+        // document.querySelector('.next').innerHTML = ''; // удалили DIV
     } else {
 
         j = j + 1;
